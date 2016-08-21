4b5eceac3cc78e4f115fe852de5242ab07196af8
fengdasuk19 <fengdasuk19@gmail.com>
2016-08-21 14:23:59 +0800

    Crawling test03: change bio selector, to get all p-tags

--- Crawler-CSV-160819/crawler.py
+++ Crawler-CSV-160819/crawler.py
@@ -4,27 +4,75 @@ from urllib.request import urlretrieve
 import time
 import random
 
-originURLroot = 'http://yenchingacademy.org/list/6/date/2017/p/'
+def writeln(f, content):
+    f.write(content)
+    f.write('\n')
 
-with open('test.txt', 'w') as testfile:
-    for iDigit in range(1, 13 + 1):
-        # Crawling
-        originURL = originURLroot + str(iDigit)
-        webDataRaw = requests.get(originURL)
-        webData = webDataRaw.text
-        soup = BeautifulSoup(webData, 'lxml')
+def isFemale(rawText):
+    for i in ["She ", "she ", "Her ", "her "]: # Must use SPACE following female keyword
+        if i in rawText:
+            return True
+    return False
 
-        nameBagRaw = soup.select('#cont > section > article > h4')
-        bioBagRaw = soup.select('#cont > section > article > div > p')
 
-        nameBag = [i.get_text() for i in nameBagRaw]
-        bioBag = [i.get_text() for i in bioBagRaw]
-        for nameItem, bioItem in zip(nameBag, bioBag):
-            testfile.write(nameItem)
-            testfile.write(', ')
-            testfile.write(bioItem)
-            testfile.write('\n')
+originURLroot = 'http://yenchingacademy.org/list/6/date/2017/p/2'
+nameFile = open('nameList.txt', 'a')
+genderFile = open('genderList.txt', 'a')
+bioFile = open('bioList.txt', 'a')
 
-        print('Succeed for page {}.\n'.format(iDigit))
+for iDigit in range(1, 13 + 1):
+    # Crawling
+    originURL = originURLroot# + str(iDigit)
+    webDataRaw = requests.get(originURL)
+    webData = webDataRaw.text
+    soup = BeautifulSoup(webData, 'lxml')
 
-        time.sleep(random.randint(1, 5))
+    nameBagRaw = soup.select('#cont > section > article > h4')
+    bioBagRaw = soup.select('#cont > section > article > div') # before '#cont > section > article > div > p'
+    for i in bioBagRaw:
+        print(i)
+
+    print()
+    input("Enter to continue.")
+
+    nameBag = [i.get_text() for i in nameBagRaw]
+    bioBag = [i.get_text() for i in bioBagRaw]
+    for i in bioBag:
+        if i == '':
+            bioBag.remove(i)
+    print('bioBag, nameBag')
+    for i in bioBag:
+        print(i)
+    print(len(nameBag))
+    input("Enter to continue.")
+    if len(nameBag) == len(bioBag):
+        for name, bio in zip(nameBag, bioBag):
+            writeln(nameFile, name)
+            writeln(bioFile, bio)
+            if isFemale(bio):
+                writeln(genderFile, 'Female')
+                print('Female\n')
+            else:
+                writeln(genderFile, 'Male')
+                print('Male\n')
+
+            print('{} finished.'.format(name))
+    else:
+        print('False')
+
+    print('Succeed for page {}.\n'.format(iDigit))
+
+
+'''
+    for name, bio in zip(nameBag, bioBag):
+        writeln(nameFile, name)
+        writeln(bioFile, bio)
+        if isFemale(bio):
+            writeln(genderFile, 'Female')
+            print('Female\n')
+        else:
+            writeln(genderFile, 'Male')
+            print('Male\n')
+
+        print('{} finished.'.format(name))
+'''
