cfcba884f192b202d4e5323ef734673e5342ae2c
fengdasuk19 <fengdasuk19@gmail.com>
2016-08-21 12:48:10 +0800

    Crawling test02: bio get; output succeed

--- Crawler-CSV-160819/crawler.py
+++ Crawler-CSV-160819/crawler.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.request import urlretrieve
 import time
+import random
 
 originURLroot = 'http://yenchingacademy.org/list/6/date/2017/p/'
 
@@ -13,13 +14,17 @@ with open('test.txt', 'w') as testfile:
         webData = webDataRaw.text
         soup = BeautifulSoup(webData, 'lxml')
 
-        nameBag = soup.select('#cont > section > article > h4')
-        print(nameBag)
+        nameBagRaw = soup.select('#cont > section > article > h4')
+        bioBagRaw = soup.select('#cont > section > article > div > p')
 
-        time.sleep(1)
-        # cont > section > article.clearfix.v536 > h4
+        nameBag = [i.get_text() for i in nameBagRaw]
+        bioBag = [i.get_text() for i in bioBagRaw]
+        for nameItem, bioItem in zip(nameBag, bioBag):
+            testfile.write(nameItem)
+            testfile.write(', ')
+            testfile.write(bioItem)
+            testfile.write('\n')
 
-        # cont > section > article.clearfix.v536.on > div > p:nth-child(1)
-        # cont > section > article.clearfix.v491.on > div > p
-        # cont > section > article.clearfix.v534.on > div > p
+        print('Succeed for page {}.\n'.format(iDigit))
 
+        time.sleep(random.randint(1, 5))
